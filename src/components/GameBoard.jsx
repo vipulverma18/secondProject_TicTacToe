ec89084999f74f2abe7486b69934d43c7e7ff174
Vipul Verma <vipulverma18260@gmail.com>
2024-02-09 01:25:23 +0530

    Testing

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,7 +2,7 @@
 
 const initialGameBoard=[[null,null,null],[null,null,null],[null,null,null]]
 
-export default function(){
+export default function GameBoard(){
 return(
     <ol id='game-board'>
         {initialGameBoard.map((row,rowIndex)=>(<li key={rowIndex}>
